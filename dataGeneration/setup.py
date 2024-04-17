a57891b6d8a37c8a2a41f9722c3944f1ef4bda60
arsereg <arsereg@gmail.com>
2024-04-17 00:18:03 -0600

    Adding requirements.txt and setup.py

---
+++ dataGeneration/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dataGeneration',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ginomarin',
+    author_email='arsereg@gmail.com',
+    description='Tool built to create a list of data'
+)
